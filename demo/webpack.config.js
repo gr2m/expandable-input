3e20506674c52d03e15bd91033dfb7abe79c581d
Gregor Martynus <gregor@martynus.net>
2016-02-06 17:14:06 -0600

    chore: moved demo files to demo/

---
+++ demo/webpack.config.js
@@ -0,0 +1,21 @@
+var ExtractTextPlugin = require('extract-text-webpack-plugin')
+
+module.exports = {
+  entry: './entry.js',
+  output: {
+    path: __dirname,
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.css$/,
+        // loader: 'style-loader!css-loader'
+        loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin('bundle.css')
+  ]
+}
