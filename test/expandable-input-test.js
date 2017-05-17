7fda55361d67e5efab8fb7ff96818571d96c5873
Gregor <gregor@martynus.net>
2017-05-17 23:36:24 +0200

    test: use selsa for testing

--- test/expandable-input-test.js
+++ test/expandable-input-test.js
@@ -1,11 +1,11 @@
-/* global describe, beforeEach, it, $ */
-
-// we delete the cached require of '@gr2m/frontend-test-setup'
-// to make it work with mocha --watch.
-delete require.cache[require.resolve('@gr2m/frontend-test-setup')]
-require('@gr2m/frontend-test-setup')
+/* global $ */
+var spawn = require('child_process').spawn
 
 var expect = require('chai').expect
+var selsa = require('selsa')
+var test = require('tap').test
+
+var selsaOptions = {}
 
 function toValue (result) {
   if (isError(result.value)) {
@@ -23,69 +23,85 @@ function isError (value) {
   return value && value.name && /error/i.test(value.name)
 }
 
-describe('=== expandable-input ===', function () {
-  this.timeout(90000)
+selsa(selsaOptions, function (error, api) {
+  if (error) {
+    throw error
+  }
 
-  beforeEach(function () {
-    return this.client.url('/')
-  })
+  spawn('npm', ['start', '--', '--port', '3891'])
 
-  it('sanity check', function () {
-    return this.client
-      .url('/index.html')
-      // .getTitle()
-      .execute(function getTitle () {
-        return document.title
-      }).then(toValue)
-      .then(function (title) {
-        expect(title).to.equal('Expandable Input – A jQuery plugin')
+  setTimeout(function () {
+    console.log('ready?')
+
+    test('=== expandable-input ===', function (group) {
+      group.beforeEach(function () {
+        return api.browser.url('http://localhost:3891/')
       })
-  })
 
-  it('inline input grows with content', function () {
-    var defaultWidth
+      group.test('sanity check', function (t) {
+        api.browser
+          .url('http://localhost:3891/index.html')
+          // .getTitle()
+          .execute(function getTitle () {
+            return document.title
+          }).then(toValue)
+          .then(function (title) {
+            expect(title).to.equal('Expandable Input – A jQuery plugin')
+          })
+          .then(t.end, t.end)
+      })
 
-    return this.client
-      .execute(function setNameAndGetWidth () {
-        return $('[name=name]').width()
-      }).then(toValue)
-      .then(function (width) {
-        defaultWidth = width
-      })
-      // click also sets focus, so "keys" command enters text into the input
-      .click('[name="name"]')
-      .keys('Missy Misdemour Elliot')
-      .execute(function setNameAndGetWidth () {
-        return $('[name=name]').width()
-      }).then(toValue)
-      .then(function (width) {
-        expect(width).to.not.equal(defaultWidth)
-      })
-  })
+      group.test('inline input grows with content', function (t) {
+        var defaultWidth
 
-  it('inline input grows when set with .val(text)', function () {
-    return this.client
-      .execute(function setNameAndGetWidth () {
-        var $input = $('[name=name]')
-        var defaultWidth = $input.width()
-        $input.text('Missy Misdemour Elliot')
-        return defaultWidth !== $input.width()
-      }).then(toValue)
-      .then(function (hasDifferentSize) {
-        expect(hasDifferentSize).to.equal(true)
+        api.browser
+          .execute(function setNameAndGetWidth () {
+            return $('[name=name]').width()
+          }).then(toValue)
+          .then(function (width) {
+            defaultWidth = width
+          })
+          // click also sets focus, so "keys" command enters text into the input
+          .click('[name="name"]')
+          .keys('Missy Misdemour Elliot')
+          .execute(function setNameAndGetWidth () {
+            return $('[name=name]').width()
+          }).then(toValue)
+          .then(function (width) {
+            expect(width).to.not.equal(defaultWidth)
+          })
+          .then(t.end, t.end)
       })
-  })
 
-  it('block input grows with content', function () {
-    return this.client
-      .execute(function setNameAndGetWidth () {
-        var $input = $('[name=about]')
-        var defaultHeight = $input.height()
-        $input.html('super<br>funky<br>fresh')
-        return defaultHeight !== $input.height()
-      }).then(toValue)
-      .then(function (hasDifferentSize) {
-        expect(hasDifferentSize).to.equal(true)
+      group.test('inline input grows when set with .val(text)', function (t) {
+        api.browser
+          .execute(function setNameAndGetWidth () {
+            var $input = $('[name=name]')
+            var defaultWidth = $input.width()
+            $input.text('Missy Misdemour Elliot')
+            return defaultWidth !== $input.width()
+          }).then(toValue)
+          .then(function (hasDifferentSize) {
+            expect(hasDifferentSize).to.equal(true)
+          })
+          .then(t.end, t.end)
       })
-  })
+
+      group.test('block input grows with content', function (t) {
+        api.browser
+          .execute(function setNameAndGetWidth () {
+            var $input = $('[name=about]')
+            var defaultHeight = $input.height()
+            $input.html('super<br>funky<br>fresh')
+            return defaultHeight !== $input.height()
+          }).then(toValue)
+          .then(function (hasDifferentSize) {
+            expect(hasDifferentSize).to.equal(true)
+          })
+          .then(t.end, t.end)
+      })
+
+      group.end()
+    })
+  }, 3000)
 })
